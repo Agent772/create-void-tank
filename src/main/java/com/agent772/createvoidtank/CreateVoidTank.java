f5c462d0a14902e1fed20a6d5987314faf6adcee
Matej Hozlar <hozlarmatej0@gmail.com>
2026-04-23 17:20:30 +0000

    feat: initial project setup for Create Void Tank mod
    NeoForge 1.21.1 / Create 6 mod skeleton with Gradle build, CurseForge
    and Modrinth release tasks, and a bare-bones main mod class.

---
+++ src/main/java/com/agent772/createvoidtank/CreateVoidTank.java
@@ -0,0 +1,20 @@
+package com.agent772.createvoidtank;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(CreateVoidTank.MODID)
+public class CreateVoidTank {
+    public static final String MODID = "createvoidtank";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateVoidTank(IEventBus modEventBus, ModContainer modContainer) {
+        LOGGER.info("Create Void Tank mod initialized!");
+    }
+}
