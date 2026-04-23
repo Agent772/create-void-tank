efff76783769e811c0a1b65735dd44b49ccde2d0
Matej Hozlar <hozlarmatej0@gmail.com>
2026-04-23 20:30:03 +0000

    feat: add item tooltip showing activation requirement from config (#13)
    Override appendHoverText in VoidTankItem to display the configured
    activation condition (always active, requires heat, requires redstone).
    When heat mode is active, a second line shows the minimum heat level.
    Config access is guarded with SPEC.isLoaded() for pre-world safety.

--- src/main/java/com/agent772/createvoidtank/content/VoidTankItem.java
+++ src/main/java/com/agent772/createvoidtank/content/VoidTankItem.java
@@ -1,25 +1,69 @@
 package com.agent772.createvoidtank.content;
 
+import com.agent772.createvoidtank.config.ModConfig;
+import com.agent772.createvoidtank.config.ModConfig.ActivationMode;
+import com.agent772.createvoidtank.config.ModConfig.MinimumHeatLevel;
 import com.agent772.createvoidtank.content.voidtank.VoidTankBlockEntity;
 import com.agent772.createvoidtank.registry.ModBlockEntities;
 import com.simibubi.create.api.connectivity.ConnectivityHandler;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.List;
+
 public class VoidTankItem extends BlockItem {
 
     public VoidTankItem(Block block, Properties properties) {
         super(block, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context,
+                                List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+
+        if (!ModConfig.SPEC.isLoaded())
+            return;
+
+        ActivationMode mode = ModConfig.ACTIVATION_MODE.get();
+
+        switch (mode) {
+            case ALWAYS_ACTIVE -> tooltipComponents.add(
+                    Component.translatable("createvoidtank.tooltip.activation.always_active")
+                            .withStyle(ChatFormatting.DARK_GREEN));
+            case REQUIRES_HEAT -> {
+                tooltipComponents.add(
+                        Component.translatable("createvoidtank.tooltip.activation.requires_heat")
+                                .withStyle(ChatFormatting.GOLD));
+                MinimumHeatLevel heatLevel = ModConfig.MINIMUM_HEAT_LEVEL.get();
+                String heatKey = switch (heatLevel) {
+                    case SMOULDERING -> "createvoidtank.tooltip.activation.heat.smouldering";
+                    case KINDLED -> "createvoidtank.tooltip.activation.heat.kindled";
+                    case SEETHING -> "createvoidtank.tooltip.activation.heat.seething";
+                };
+                tooltipComponents.add(
+                        Component.translatable("createvoidtank.tooltip.activation.heat_level",
+                                        Component.translatable(heatKey))
+                                .withStyle(ChatFormatting.GRAY));
+            }
+            case REQUIRES_REDSTONE -> tooltipComponents.add(
+                    Component.translatable("createvoidtank.tooltip.activation.requires_redstone")
+                            .withStyle(ChatFormatting.RED));
+        }
     }
 
     @Override
